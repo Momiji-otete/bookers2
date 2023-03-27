f8a40f00774a3cc63b55a250e0b7e31191e5503b
Momiji-otete <126852371+Momiji-otete@users.noreply.github.com>
2023-03-27 07:23:28 +0000

    [Update]indexのテーブルタグ部分作成

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,10 +1,26 @@
 class BooksController < ApplicationController
   def new
+    @book = Book.new
+  end
+
+  def create
+    @book = Book.new(book_params)
+    @book.user_id = current_user.id
+    @book.save
+    #あとでbookのshowに変更する
+    redirect_to books_path
   end
 
   def index
+    @books = Book.all
   end
 
   def show
+  end
+
+
+  private
+  def book_params
+    params.require(:book).permit(:title, :body)
   end
 end
